5687b4317776be213848a4ac0c8158e5ce4ccc58
Dennis Murage <dmurage140@gmail.com>
2023-08-07 19:17:04 +0300

    Added a docstring

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
-
+"""_summary_"""
 from typing import List, TypeVar
 
 class Auth:
-
+    """_summary_
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """_summary_
+
+        Args:
+            path (str): _description_
+            excluded_paths (List[str]): _description_
+
+        Returns:
+            bool: _description_
+        """
         if path is None or excluded_paths is None:
             return True
         if len(excluded_paths) == 0:
@@ -18,6 +28,14 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
+        """_summary_
+
+        Args:
+            request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
         if request is None:
             return None
         if "Authorization" not in request.keys():
@@ -26,4 +44,9 @@ class Auth:
         return request["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return None
