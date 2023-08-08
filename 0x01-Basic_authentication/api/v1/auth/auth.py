0958c08d1295ffa2788adff43b4bc3bd9a94c5fc
Dennis Murage <dmurage140@gmail.com>
2023-08-08 14:36:30 +0300

    Implementin path/*

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,6 +27,11 @@ class Auth:
         if plus in excluded_paths:
             return False
 
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path.strip("*")):
+                return False
+            if plus.startswith(excluded_path.strip("*")):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
