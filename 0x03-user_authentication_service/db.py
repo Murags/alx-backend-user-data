6f47e0905f6201672599284cb758fb691d6c601a
Dennis Murage <dmurage140@gmail.com>
2023-08-16 15:11:12 +0300

    Refactoring

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,10 +35,9 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a user to the database"""
-        session = self._session
         new_user = User(email=email, hashed_password=hashed_password)
-        session.add(new_user)
-        session.commit()
+        self._session.add(new_user)
+        self._session.commit()
 
         return new_user
 
